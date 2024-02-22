c322673c97ed5574ff6ae866a26bf5b998810823
BrayanGomezL <157052014+BrayanGomezL@users.noreply.github.com>
2024-02-21 20:07:57 -0600

    Update Game_Of_Progra.py

--- Game_Of_Progra.py
+++ Game_Of_Progra.py
@@ -1,4 +1,4 @@
-monto= float(input("Ingrese el monto que desea solisitar: "))
+monto= float(input("Ingrese el monto que desea solicitar: "))
 plazo= int(input("Ingrese un plazo de 6 a 60 meses: "))
 intereses_anuales= 0.20
 intereses_mensuales= intereses_anuales/ 12
