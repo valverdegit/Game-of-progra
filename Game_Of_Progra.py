edd9ceaa30bdc87d152f3642e892d357886c6557
Andrey <35713619+ANDREYOA@users.noreply.github.com>
2024-02-21 20:05:53 -0600

    Add files via upload

---
+++ Game_Of_Progra.py
@@ -0,0 +1,5 @@
+monto= float(input("Ingrese el monto que desea solisitar: "))
+plazo= int(input("Ingrese un plazo de 6 a 60 meses: "))
+intereses_anuales= 0.20
+intereses_mensuales= intereses_anuales/ 12
+
