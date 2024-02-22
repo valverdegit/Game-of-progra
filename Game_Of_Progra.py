2a5c4a7d8ea40cbf6286e4274fb9a22a632433ca
valverdegit <157626638+valverdegit@users.noreply.github.com>
2024-02-21 20:12:00 -0600

    Update Game_Of_Progra.py

--- Game_Of_Progra.py
+++ Game_Of_Progra.py
@@ -4,3 +4,10 @@ intereses_anuales= 0.20
 intereses_mensuales= intereses_anuales/ 12
 
 cuotaMensual = (monto/plazo) = intereses_mensuales
+mesesCorrecto=True
+while mesesCorrecto==True:
+    plazoMeses=int(input("Porfavor digite el tiempo en meses que desea que dure el prestamo:\nSolose permiten entre 6 y 60"))
+    if plazoMeses>=6 and plazoMeses<=60:
+        mesesCorrecto=False
+    else:
+        print("Porfavor indique un plazo entre 6 meses y 60 meses")
